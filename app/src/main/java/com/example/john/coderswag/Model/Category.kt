93ad4bcf1c246de058710d1a296f6dd1bb433839
John <Braveheart22@GMail.com>
2018-03-28 12:38:27 -0400

    Create custom layout for categories

--- app/src/main/java/com/example/john/coderswag/Model/Category.kt
+++ app/src/main/java/com/example/john/coderswag/Model/Category.kt
@@ -3,4 +3,8 @@ package com.example.john.coderswag.Model
 /**
  * Created by John on 3/28/2018.
  */
-class Category (val title: String, val image: String)
+class Category (val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
