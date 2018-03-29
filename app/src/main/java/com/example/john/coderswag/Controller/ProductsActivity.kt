b8fd83ae43c89c9a90eae107acc84c4c84d67797
John Strack <Braveheart22@GMail.com>
2018-03-29 12:05:03 -0400

    Created products activity

---
+++ app/src/main/java/com/example/john/coderswag/Controller/ProductsActivity.kt
@@ -0,0 +1,17 @@
+package com.example.john.coderswag.Controller
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.john.coderswag.R
+import com.example.john.coderswag.Utilities.EXTRA_CATEGORY
+
+class ProductsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_products)
+
+        val categoryType = intent.getStringArrayExtra(EXTRA_CATEGORY)
+        println(categoryType)
+    }
+}
